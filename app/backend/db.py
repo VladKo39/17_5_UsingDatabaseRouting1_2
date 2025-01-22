97805d3974549cda350d777fc259f756c26ce48a
VladKo39 <vladko67@mail.ru>
2025-01-22 14:14:14 +0200

    Delete app directory

--- app/backend/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
-
-engine=create_engine('sqlite:///taskmanager1.db',echo=True)
-SessionLocal=sessionmaker(bind=engine)
-
-class Base(DeclarativeBase):
-    pass
